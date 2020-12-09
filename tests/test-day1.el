16d49cb2f30dea0f1b65b73173e5d943bbe1e236
coffeepenbit <coffeepenbit@gmail.com>
2020-12-08 21:29:11 -0500

    Initial commit.

---
+++ tests/test-day1.el
@@ -0,0 +1,51 @@
+;;; test-day1.el --- Day 1 Advent of Code 2020            -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  kga
+
+;; Author: kga <kga@Thinkpad-OpenSUSE>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+(load-file "../day1.el")
+
+(ert-deftest day1-test-part1-solution ()
+  (should (equal 1006875 (day1-part1-solution))))
+
+
+(ert-deftest day1-test--passes-criteria-p ()
+  (should (equal t (day1--passes-criteria-p (list 1 2) (list 2019 1)))))
+
+
+(ert-deftest day1-test--indices-are-unique-p ()
+  (should (equal nil (day1--indices-are-unique-p '(1 1))))
+  (should (equal t (day1--indices-are-unique-p '(1 2))))
+  (should (equal nil (day1--indices-are-unique-p '(1 1 2))))
+  (should (equal t (day1--indices-are-unique-p '(1 2 3)))))
+
+
+(ert-deftest day1-test--elems-sum-to-2020-p ()
+  (should (equal t (day1--elems-sum-to-2020-p '(2020))))
+  (should (equal nil (day1--elems-sum-to-2020-p '(2019))))
+  (should (equal t (day1--elems-sum-to-2020-p '(2019 1)))))
+
+
+
+
+
+(provide 'test-day1)
+;;; test-day1.el ends here
---
+++ tests/test-day2.el
@@ -0,0 +1,70 @@
+;;; test-day2.el --- Day 2 Advent of Code 2020            -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  kga
+
+;; Author: kga <kga@Thinkpad-OpenSUSE>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+(load-file "../day2.el")
+
+(ert-deftest day2-test-part1-solution ()
+  (should (= 1 (let ((day2-input "1-3 c: cat"))
+                 (day2-part1-solution)))))
+
+
+(ert-deftest day2-test--password-valid-p ()
+  (should (equal t (day2--password-valid-p '((rules . (1 3))
+                                             (char . ?a)
+                                             (password . "aardvark"))))))
+
+
+(ert-deftest day2-test--char-in-string-ntimes ()
+  (should (= (day2--char-in-string-ntimes ?c "cat") 1))
+  (should (= (day2--char-in-string-ntimes ?c "dog") 0))
+  (should (= (day2--char-in-string-ntimes ?a "aardvark") 3)))
+
+
+(ert-deftest day2-test--password-valid-part2-p ()
+  (should (equal nil (day2--password-valid-part2-p '((rules . (1 3))
+                                                   (char . ?a)
+                                                   (password . "aba")))))
+  (should (equal t (day2--password-valid-part2-p '((rules . (1 3))
+                                                     (char . ?a)
+                                                     (password . "abc")))))
+  (should (equal t (day2--password-valid-part2-p '((rules . (2 3))
+                                                     (char . ?b)
+                                                     (password . "abcde"))))))
+
+
+(ert-deftest day2-test-part2-solution ()
+  (should (= 1 (let ((day2-input "1-3 c: cat"))
+                 (day2-part2-solution))))
+  (should (= 1 (let ((day2-input
+                      "1-3 c: cat
+1-3 d: dad"))
+                 (day2-part2-solution))))
+  (should (= 2 (let ((day2-input
+                      "1-3 c: cat
+1-3 d: dad
+1-3 d: daa"))
+                 (day2-part2-solution)))))
+
+
+(provide 'test-day2)
+;;; test-day2.el ends here
