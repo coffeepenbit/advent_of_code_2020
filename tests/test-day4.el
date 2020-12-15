2ab2cf5348c3f2672f092df1f323f08e074a7afd
coffeepenbit <coffeepenbit@gmail.com>
2020-12-14 21:42:10 -0500

    First five tests passing.

--- tests/test-day4.el
+++ tests/test-day4.el
@@ -24,6 +24,59 @@
 (load-file "../day4.el")
 
 
+(ert-deftest test-day4--get-passports nil
+  (let ((passports-string "byr:2024 iyr:2016
+eyr:2034 ecl:zzz pid:985592671 hcl:033b48
+hgt:181 cid:166
+
+hgt:66cm
+pid:152cm
+hcl:cfb18a eyr:1947
+byr:2020 ecl:zzz iyr:2029
+
+ecl:gry hcl:#888785 eyr:2023 cid:63
+iyr:2019 hgt:177cm
+pid:656793259")
+        (passports '("byr:2024 iyr:2016
+eyr:2034 ecl:zzz pid:985592671 hcl:033b48
+hgt:181 cid:166"
+                     "hgt:66cm
+pid:152cm
+hcl:cfb18a eyr:1947
+byr:2020 ecl:zzz iyr:2029"
+                     "ecl:gry hcl:#888785 eyr:2023 cid:63
+iyr:2019 hgt:177cm
+pid:656793259")))
+    (should (equal passports (day4--get-passports passports-string)))))
+
+
+(ert-deftest test-day4--nvalid-passports nil
+  (let ((passports '("byr:2024 iyr:2016
+eyr:2034 ecl:zzz pid:985592671 hcl:033b48
+hgt:181 cid:166"
+                     "hgt:66cm
+pid:152cm
+hcl:cfb18a eyr:1947
+byr:2020 ecl:zzz iyr:2029"
+                     "ecl:gry hcl:#888785 eyr:2023 cid:63
+iyr:2019 hgt:177cm
+pid:656793259")))
+    (should (equal 2 (day4--nvalid-pasports passports)))))
+
+
+(ert-deftest test-day4--passport-valid-p nil
+  (let ((passport "hgt:66cm
+pid:152cm
+hcl:cfb18a eyr:1947
+byr:2020 ecl:zzz iyr:2029"))
+    (should (equal t (day4--passport-valid-p passport))))
+  (let ((passport "byr:2024 iyr:2016
+eyr:2034 ecl:zzz pid:985592671 hcl:033b48
+hgt:181 cid:166"))
+    (should (equal t (day4--passport-valid-p passport))))
+  (let ((passport "byr:2024 iyr:2016 hgt:181 cid:166"))
+    (should (equal nil (day-4--passport-valid-p passport)))))
+
 
 (provide 'test-day4)
 ;;; test-day4.el ends here
